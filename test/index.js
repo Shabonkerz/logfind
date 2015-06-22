ecdedcda2dfc514026bbb71ea70847a9b5d4a94f
Asa Rudick <Shabonkerz@gmail.com>
2015-06-21 21:29:02 -0500

    add more LogFind unit tests

--- test/index.js
+++ test/index.js
@@ -4,8 +4,57 @@ var LogFind = require( '../lib/logfind' );
 var logFinder = new LogFind();
 
 describe( 'LogFind unit tests.', function () {
+    var pattern = new RegExp(/^rickshaw$/gm);
+
     it( 'LogFind.getFileMatches should return 2 files.', function () {
         var files = logFinder.getFileMatches();
-        assert( files.length == 2, 'we expected 2 files.' );
+        assert( files.length === 2, 'we expected 2 files.' );
     } );
+
+    it( 'LogFind.findInFile should return true.', function () {
+        var file = 'test.txt';
+        var found = logFinder.findInFile(pattern, file);
+        assert( found === true, 'we expected pattern to be found.' );
+    } );
+
+    it( 'LogFind.findInFile should return false.', function () {
+        var file = 'test2.txt';
+        var found = logFinder.findInFile(pattern, file);
+        assert( found === false, 'we expected pattern to not be found.' );
+    } );
+
+    it( 'LogFind.findFilesWithPattern should return array of length 0.', function () {
+        var files = ['test.txt', 'test2.txt'];
+        var pattern = new RegExp(/^slkdjklsjd$/gm);
+        var foundFiles = logFinder.findFilesWithPattern(pattern, files);
+        assert( foundFiles.length === 0, 'we expected no files in result.' );
+    } );
+
+    it( 'LogFind.findFilesWithPattern should return array of length 1.', function () {
+        var files = ['test.txt', 'test2.txt'];
+        var foundFiles = logFinder.findFilesWithPattern(pattern, files);
+        assert( foundFiles.length === 1, 'we expected only 1 file in result.' );
+    } );
+
+    it( 'LogFind.find should return array of length 0.', function () {
+        var options = {o: false};
+        var input = ['^lkjsdflskjdf$'];
+        var foundFiles = logFinder.find(options, input);
+        assert( foundFiles.length === 0, 'we expected no files in result.' );
+    } );
+
+    it( 'LogFind.find should return array of length 1 for `OR` mode.', function () {
+        var options = {o: true};
+        var input = ['something rickshaw'];
+        var foundFiles = logFinder.find(options, input);
+        assert( foundFiles.length === 1, 'we expected only 1 file in result.' );
+    } );
+
+    it( 'LogFind.find should return array of length 1.', function () {
+        var options = {o: false};
+        var input = ['^rickshaw$'];
+        var foundFiles = logFinder.find(options, input);
+        assert( foundFiles.length === 1, 'we expected only 1 file in result.' );
+    } );
+
 } );
