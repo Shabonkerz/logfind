62fec517a03e85bcfac1c2912706efe9d8cfb52c
Asa Rudick <Shabonkerz@gmail.com>
2015-06-21 21:07:28 -0500

    format unit test file

--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
 'use strict';
-var assert = require('assert');
-var LogFind = require('../lib/logfind');
+var assert = require( 'assert' );
+var LogFind = require( '../lib/logfind' );
 var logFinder = new LogFind();
 
-describe('LogFind unit tests.', function () {
-  it('LogFind.getFileMatches should return 2 files.', function () {
-      var files = logFinder.getFileMatches();
-      assert(files.length == 2, 'we expected 2 files.');
-  });
-});
+describe( 'LogFind unit tests.', function () {
+    it( 'LogFind.getFileMatches should return 2 files.', function () {
+        var files = logFinder.getFileMatches();
+        assert( files.length == 2, 'we expected 2 files.' );
+    } );
+} );
