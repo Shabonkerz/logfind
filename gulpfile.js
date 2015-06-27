73692703315cbdafd5c5586bd1acb0d898cd4141
Asa Rudick <Shabonkerz@gmail.com>
2015-06-27 16:23:56 -0500

    skip jshint-reporter step
    Since it needlessly reports jshint errors in libraries used by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,42 +7,41 @@ var istanbul = require('gulp-istanbul');
 var plumber = require('gulp-plumber');
 
 var handleErr = function (err) {
-  console.log(err.message);
-  process.exit(1);
+    console.log(err.message);
+    process.exit(1);
 };
 
 gulp.task('static', function () {
-  return gulp.src([
+    return gulp.src([
       '**/*.js',
       '!node_modules/**'
     ])
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'))
-    .pipe(jscs())
-    .on('error', handleErr);
+        .pipe(jshint('.jshintrc'))
+        // .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'))
+        .pipe(jscs())
+        .on('error', handleErr);
 });
 
 gulp.task('pre-test', function () {
-  return gulp.src('lib/**/*.js')
-    .pipe(istanbul({includeUntested: true}))
-    .pipe(istanbul.hookRequire());
+    return gulp.src('lib/**/*.js')
+        .pipe(istanbul({includeUntested: true}))
+        .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr;
+    var mochaErr;
 
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(mocha({reporter: 'spec'}))
-    .on('error', function (err) {
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', function () {
-      cb(mochaErr);
-    });
+    gulp.src('test/**/*.js')
+        .pipe(plumber())
+        .pipe(mocha({reporter: 'spec'}))
+        .on('error', function (err) {
+            mochaErr = err;
+        })
+        .pipe(istanbul.writeReports())
+        .on('end', function () {
+            cb(mochaErr);
+        });
 });
-
 
 gulp.task('default', ['static', 'test']);
